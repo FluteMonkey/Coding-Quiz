bdd2e15ab309c4967da1630e0f42d61eeab56248
Zchun <z.c.chun2003@gmail.com>
2022-05-29 18:49:21 -0500

    coded start of quiz and added timer functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,26 @@
 //variable to reference DOM elements
-var questionsEl = document.getElementById("questions");
+var questionsEl = document.getElementById("question");
 var timerEl = document.getElementById("time");
 var choicesEl = document.getElementById("choices");
 var submitBtn = document.getElementById("submit");
-var startBtn = document.getElementById("start");
+var startBtn = document.getElementById("get_started");
 var initialsEl = document.getElementById("initials");
 var feedbackEl = document.getElementById("feedback");
 
 // variables to keep track of quiz 
   var questionIndex = 0;
-  var time = questions.length
-  var timerId
+  var time = questions.length * 20;
+  var timerId;
 
   function quizStart() {
       //hiding the start screen
-
+    var startScreenEl = document.getElementById("starting-screen");
+    startScreenEl.setAttribute("class", "hidden");
       //unhide questions
-
+      questionsEl.removeAttribute("class");
       //start timer
+      timerId = setInterval(clockTick, 1000);
+      timerEl.textContent = time;
 
       getQuestion();
   }
@@ -49,3 +52,10 @@ var feedbackEl = document.getElementById("feedback");
 
       //stop timer
   }
+
+  function clockTick() {
+    time--;
+    timerEl.textContent = time;
+  }
+
+  startBtn.onclick = quizStart;
